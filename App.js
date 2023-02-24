232bfb35e8674a9e09348f19ad7843fc7f7aa105
Jason852 <62583852+Jason852@users.noreply.github.com>
2023-02-24 12:39:51 -0500

    Now will display after fetching

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
 import { useState, useEffect } from "react";
 
 export default function App() {
   const [data, setData] = useState();
+  const [periods, setPeriods] = useState();
 
   useEffect(() => {
     fetch("https://api.weather.gov/gridpoints/LWX/97,71/forecast")
@@ -21,14 +22,20 @@ export default function App() {
   console.log({ data });
 
   return (
-    <View style={styles.container}>
-      <Text>
-        This is about to be the most AWESOMEST Weather App in the world! At
-        least almost the most awesomest. Check Check
-      </Text>
-      <Text style={{ color: "red" }}>{data.type}</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      {data ? (
+        <View style={styles.container}>
+          <Text>
+            This is about to be the most AWESOMEST Weather App in the world! At
+            least almost the most awesomest. Check Check
+          </Text>
+          <Text style={{ color: "red" }}>{data.properties.updated}</Text>
+          <StatusBar style="auto" />
+        </View>
+      ) : (
+        <ActivityIndicator />
+      )}
+    </>
   );
 }
 
