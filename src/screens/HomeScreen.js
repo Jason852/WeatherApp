f9b046103245423e03314722539c5bf8d71b628c
Jason852 <62583852+Jason852@users.noreply.github.com>
2024-03-06 23:50:16 -0500

    fetched data

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,24 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
+
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=26.1276&lon=-80.2331&appid=5edd63147368df5ac8a2b0e22f443d0c&units=imperial`;
 
 export default function HomeScreen() {
+  const fetchWeather = async () => {
+    console.log("fetch data ");
+
+    const results = await fetch(url);
+    const data = await results.json();
+    console.log(JSON.stringify(data, null, 2));
+  };
+
+  useEffect(() => {
+    fetchWeather();
+  }, []);
+
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <Text>Heres where its all about to go down. on the spot</Text>
     </View>
   );
 }
