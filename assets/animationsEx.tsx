c57a6f3d4a7dd860f712da41969f66608df455ab
Jason852 <62583852+Jason852@users.noreply.github.com>
2024-04-09 15:26:45 -0400

    finished adding animations

--- assets/animationsEx.tsx
+++ assets/animationsEx.tsx
@@ -1,75 +1,75 @@
 export const animationSelection = [
     {
         id: "01d",
-        animation: require("./lottie/sunny.json"),
+        animation: require("./lottie/clear-sky.json"),
 
     },
-    // {
-    //     id: "01n",
-    //     animation: require("./lottie/sunnyN.json"),
-    // },
-    // {
-    //     id: "02d",
-    //     animation: require("./lottie/fewclouds.json"),
-    // },
-    // {
-    //     id: "02n",
-    //     animation: require("./lottie/fewcloudsN.json"),
-    // },
-    // {
-    //     id: "03d",
-    //     animation: require("./lottie/scattclouds.json"),
-    // },
-    // {
-    //     id: "03n",
-    //     animation: require("./lottie/scattcloudsN.json"),
-    // },
-    // {
-    //     id: "04d",
-    //     animation: require("./lottie/brokenclouds.json"),
-    // },
-    // {
-    //     id: "04n",
-    //     animation: require("./lottie/brokencloudsN.json"),
-    // },
-    // {
-    //     id: "09d",
-    //     animation: require("./lottie/rainshower.json"),
-    // },
-    // {
-    //     id: "09n",
-    //     animation: require("./lottie/rainshowerN.json"),
-    // },
-    // {
-    //     id: "10d",
-    //     animation: require("./lottie/rain.json"),
-    // },
-    // {
-    //     id: "10n",
-    //     animation: require("./lottie/rainN.json"),
-    // },
-    // {
-    //     id: "11d",
-    //     animation: require("./lottie/thunderstorm.json"),
-    // },
-    // {
-    //     id: "11n",
-    //     animation: require("./lottie/thunderstormN.json"),
-    // },
-    // {
-    //     id: "13d",
-    //     animation: require("./lottie/snow.json"),
-    // },
-    // {
-    //     id: "13n",
-    //     animation: require("./lottie/snowN.json"),
-    // },
-    // {
-    //     id: "50d",
-    //     animation: require("./lottie/mist.json"),
-    // },
-    // {
-    //     id: "50n",
-    //     animation: require("./lottie/mistN.json"),
-    // },
+    {
+        id: "01n",
+        animation: require("./lottie/clear-skyN.json"),
+    },
+    {
+        id: "02d",
+        animation: require("./lottie/few-clouds.json"),
+    },
+    {
+        id: "02n",
+        animation: require("./lottie/few-cloudsN.json"),
+    },
+    {
+        id: "03d",
+        animation: require("./lottie/cloudy.json"),
+    },
+    {
+        id: "03n",
+        animation: require("./lottie/cloudyN.json"),
+    },
+    {
+        id: "04d",
+        animation: require("./lottie/cloudy.json"),
+    },
+    {
+        id: "04n",
+        animation: require("./lottie/cloudyN.json"),
+    },
+    {
+        id: "09d",
+        animation: require("./lottie/rain-shower.json"),
+    },
+    {
+        id: "09n",
+        animation: require("./lottie/rain-showerN.json"),
+    },
+    {
+        id: "10d",
+        animation: require("./lottie/rain-shower.json"),
+    },
+    {
+        id: "10n",
+        animation: require("./lottie/rain-showerN.json"),
+    },
+    {
+        id: "11d",
+        animation: require("./lottie/thunder-storm.json"),
+    },
+    {
+        id: "11n",
+        animation: require("./lottie/thunder-stormN.json"),
+    },
+    {
+        id: "13d",
+        animation: require("./lottie/snow.json"),
+    },
+    {
+        id: "13n",
+        animation: require("./lottie/snowN.json"),
+    },
+    {
+        id: "50d",
+        animation: require("./lottie/mist.json"),
+    },
+    {
+        id: "50n",
+        animation: require("./lottie/mistN.json"),
+    },
 ];
